7323a6a4870f0c0e56719f303900eaf7f8d28a19
mattnorthcutt <matt_northcutt@icloud.com>
2025-02-28 19:12:21 -0600

    joke

--- public/main.js
+++ public/main.js
@@ -2,44 +2,36 @@
 // import ViewDirectorBasedOnUserAuthStatus from './utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
-import {
-  getRequest, postRequest, patchRequest, deleteRequest
-} from '../api/promises';
+import getRequest from '../api/promises';
 
-const htmlStructure = () => {
+const jokeHTML = () => {
   document.querySelector('#app').innerHTML = `
-    <h1>TESTING PROMISES</h1>
-    <h2>Open your dev tools</h2><br />
-    <button class="btn btn-warning" id="get-joke">GET JOKE</button>
-    <button class="btn btn-info" id="post-name">POST YOUR NAME</button>
-    <button class="btn btn-success" id="patch-name">PATCH YOUR NAME</button>
-    <button class="btn btn-danger" id="delete-name">DELETE YOUR NAME</button>
+  <h2>READY FOR A JOKEE</h2>
+  <button class="btn btn-danger" id="clicketh">HEHEHEHEHEHEHE CLICKETH ME</button><br />
+    <hr />
+    <div id="text">
+    </div>
+    <div id="punch"> 
+    </div>
   `;
+
+  let punchline = '';
+
+  document.querySelector('#app').addEventListener('click', (e) => {
+    if (e.target && e.target.id === 'clicketh') {
+      getRequest().then((response) => {
+        document.querySelector('#punch').innerHTML = '';
+        document.querySelector('#text').innerHTML = response.setup;
+        e.target.innerHTML = 'Get Punchline';
+        e.target.setAttribute('id', 'punchline');
+        punchline = response.delivery;
+      });
+    } else if (e.target && e.target.id === 'punchline') {
+      document.querySelector('#punch').innerHTML = punchline;
+      e.target.setAttribute('id', 'clicketh');
+      e.target.innerHTML = 'Get Another Joke';
+    }
+  });
 };
 
-const events = () => {
-  document.querySelector('#get-joke').addEventListener('click', () => {
-    getRequest().then(console.warn);
-  });
-  document.querySelector('#post-name').addEventListener('click', () => {
-    // update this object with your name
-    const payload = { name: 'YOUR NICKNAME' };
-    postRequest(payload).then(console.warn);
-  });
-  document.querySelector('#patch-name').addEventListener('click', () => {
-    // update this object with your name and firebaseKey that was logged in the console when you created your name in the database
-    const payload = { name: 'YOUR FULL NAME', firebaseKey: '' };
-    patchRequest(payload).then(console.warn);
-  });
-  document.querySelector('#delete-name').addEventListener('click', () => {
-    const firebaseKey = '';
-    deleteRequest(firebaseKey).then(console.warn);
-  });
-};
-
-const startApp = () => {
-  htmlStructure();
-  events(); // ALWAYS LAST
-};
-
-startApp();
+jokeHTML();
